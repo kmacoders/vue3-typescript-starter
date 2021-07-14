2bfc45944935658f0687be2c221c4312f9140708
Huwng <huwng@Neos-MacBook-Pro-1719.local>
2021-07-14 16:58:49 +0700

    test: typing

--- src/components/HelloWorldTwo.vue
+++ src/components/HelloWorldTwo.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="hello">
     <h1>{{ msg }} {{title}}</h1>
+    <h1>{{ rio.firstName }}</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref } from 'vue';
+import { IPerson } from 'Types/index';
 
 export default defineComponent({
   props: {
-    msg: String
+    msg: String,
   },
   setup() {
-    const title = ref('Vue3 Typescript')
+    const title = ref('Vue3 Typescript');
+    const rio: IPerson = reactive({
+      firstName: 'Hung',
+      lastName: 'Phung',
+      age: 4,
+    });
 
     return {
-      title
-    }
+      title,
+      rio,
+    };
   },
-})
+});
 </script>
-
 
 <style lang="scss" scoped>
 .hello {
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export interface IPerson {
+  firstName: string,
+  lastName: string,
+  age: number,
+}
